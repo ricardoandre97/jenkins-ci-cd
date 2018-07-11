709ad61bed541921618945d3bb4b467ae31099eb
Ricardo Gonzalez <ricardo.gonzalez@i2btech.com>
2018-07-11 10:37:27 -0500

    Adds things

---
+++ build/mvn-test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "################
+echo "*** Testing  ***"
+echo "################"
+
+docker run --rm -v /root/.m2:/root/.m2 -v $PWD/build/simple-java-maven-app:/app -w /app maven:3-alpine "$@"
