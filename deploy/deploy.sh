f3d721598f02fd7d46e2b39aec314c9d08beb8b6
Ricardo Gonzalez <ricardo.gonzalez@i2btech.com>
2018-07-11 14:29:39 -0500

    Adds thingsusushus

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+#  Get current vars
+echo $PASS > /tmp/.auth
+echo $BUILD_TAG >> /tmp/.auth
+scp -i /opt/key /tmp/.auth centos@linuxfacilito.online:~/.auth
 ssh -i /opt/key centos@linuxfacilito.online '~/publish'
